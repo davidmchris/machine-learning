3437ed333077ca9c0e04a5f78d7d8363a1ae0f86
David Christensen <davidmchris@gmail.com>
2016-10-23 21:50:40 -0700

    Random path for smartcab.

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -11,6 +11,9 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.actions = self.env.valid_actions  # [None, 'forward', 'left', 'right']
+        self.state = None
+
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -23,9 +26,10 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
+
         
         # TODO: Select action according to your policy
-        action = None
+        action = random.choice(self.actions)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
@@ -41,7 +45,7 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
